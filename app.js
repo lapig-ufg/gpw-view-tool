7864da1399e69875fdc79e4a0e9d7074a7855fc9
irtharles <irtharles@gmail.com>
2024-10-04 21:06:59 +0000

    Updated app.js (added 1 line, deleted 1 line).

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var data = require("users/irtharles/gpw-view-tool:data.js");
 
 // Importar o pacote palettes
 var palettes = require("users/gena/packages:palettes");
-print(palettes)
+print(palettes.d3)
 
 /**
  * Inicializa o app.
