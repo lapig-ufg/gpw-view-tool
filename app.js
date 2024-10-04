1a4986a2e0e1a15b48d2d506d98d2b2d799a2c55
irtharles <irtharles@gmail.com>
2024-10-04 23:36:09 +0000

    Updated app.js (added 2 lines, deleted 2 lines).

--- app.js
+++ app.js
@@ -39,8 +39,8 @@ function initializeApp() {
 
   // Adicionar painéis ao mapa
   var mapGrid = ui.createMapGrid(MapLayer, MainPanel);
-  ui.widgets().reset([mapGrid]);
-  ui.setLayout(ui.Panel.Layout.Flow('vertical'));
+  ui.root.widgets().reset([mapGrid]);
+  ui.root.setLayout(ui.Panel.Layout.Flow("vertical"));
 
   // Configurar evento de clique no mapa
   MapLayer.onClick(map.getValues);
